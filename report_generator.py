a58be44502846e8c66b75f314d44ecc4cb068755
artom <artom@lemon.dlink>
2025-07-06 00:49:01 +0300

    compose serving

--- report_generator.py
+++ report_generator.py
@@ -11,7 +11,10 @@ def build_url(report_filename):
 def run_Goaccess( file_path, report_name): 
     args=  ['goaccess', file_path, "-a", "-o", f"{settings.REPORTS_DIR}/{report_name}", "--log-format", "COMBINED",  ]  # write to ././report.html
     #args=  ['goaccess', file_path, "-a", "-o", "html", "--log-format", "COMBINED",  ] # write to stdout
- 
+    
+    print (f"file_path {file_path}")
+    print (f"REPORTS_DIR  {settings.REPORTS_DIR}/{report_name}")
+
     result =  subprocess.run(
         args,
         capture_output=True,
