f67b3de91f0dd4e934c6c077424bbdc728bc9504
artomartom <1tematematema@gmail.com>
2025-07-27 05:58:56 +0300

    split post+get

--- bench.py
+++ bench.py
@@ -1,14 +1,12 @@
-
-
 import time
 import re
-
+import subprocess
 def bench(data):
     copy = "./writefile" 
 
 
     start = time.time()            
-    regex = "Jul/2025:11:[345]\d"
+    regex = "Jul/2025:11:[345]"
     with open(copy, 'w') as writef:
         for line in data.split('\n'):
             if re.search(regex, line):
@@ -17,3 +15,25 @@ def bench(data):
 
     end = time.time()
     return end - start
+
+
+
+if __name__ == '__main__':
+
+
+    args =  ['goaccess', "/home/kiwi/logs/test.log" , "-a", "--log-format", "COMBINED" ] 
+
+    result =  subprocess.run(
+        args,
+        capture_output=True,
+        encoding="utf-8",
+        text=True
+    )
+    
+    print(result.stdout)
+
+
+
+
+
+
