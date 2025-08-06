b22c3412b686d8490326b2882211fd9d14ee7613
artom_artom <97805658+artomartom@users.noreply.github.com>
2025-08-06 22:25:15 +0300

    Update format_parser.py IPV6

--- format_parser.py
+++ format_parser.py
@@ -13,7 +13,7 @@ import re
 format_list =  [
     ( r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4} - - \[[0-9][0-9]\/[A-Z][a-z][a-z]\/202[0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9] \+0[0-9]00] \"(GET|HEAD|PUT|POST|DELETE|PATCH) \/.* HTTP\/[0123].[012689]\" [1-5][0-9][0-9] \d+ \"http(s|):\/\/.*\/*\"" , 
     "%h %^[%d:%t %^] \"%r\" %s %b \"%R\" \"%u\"  %^",  "%d/%b/%Y",  "%T" , "combined" ),
-    (r"((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4} - - \[[0-9][0-9]\/[A-Z][a-z][a-z]\/202[0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9] \+0[0-9]00 - (\d+.\d+|\-)] [1-5][0-9][0-9] \"(GET|HEAD|PUT|POST|DELETE|PATCH) \/.* HTTP\/[0123].[012689]\" \d+ \"(http(s|)://.*\/*|\-)\" \"(.*(ozilla|indows|pple|pera|acebook|oogle|bot|BOT|Gecko|rawler|hrome|irefox).*)\" \"(((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}|\-)\"",
+    (r"((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4} - - \[[0-9][0-9]\/[A-Z][a-z][a-z]\/202[0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9] \+0[0-9]00 - (\d+.\d+|\-)] [1-5][0-9][0-9] \"(GET|HEAD|PUT|POST|DELETE|PATCH) \/.* HTTP\/[0123].[012689]\" \d+ \"(http(s|)://.*\/*|\-)\" \"(.*(ozilla|indows|pple|pera|acebook|oogle|bot|BOT|Gecko|rawler|hrome|irefox).*)\" \"(((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}|\-|((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4}))\"",
     "%h - %^ [%d:%t - %^] %s \"%r\" %b \"%R\" \"%u\" %^","%d/%b/%Y",'%H:%M:%S',"bitrixvm main")
     ]
 
@@ -53,4 +53,5 @@ if __name__ == "__main__":
     format = Format()
     line = ('78.178.85.171 - - [02/Jul/2025:00:02:25 +0300] "GET /image/catalog/import/peregorodki-i-steny-iz-sehndvich-panelej-30me-2588.webp HTTP/1.1" 200 512156 "https://ryazan.mpaneli.ru/" "Mozilla/5.0 (Linux; arm_64; Android 12; NCO-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.6723.1823 YaApp_Android/24.120.1 YaSearchBrowser/24.120.1 BroPP/1.0 SA/3 Mobile Safari/537.36"')
     print(Format.match_line(line))
+
      
