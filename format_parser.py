3598045e60bf97600cb512735527d80faf469a96
papaya <papaya@papaya.Dlink>
2025-12-28 01:14:30 +0300

    regex referer fix + get foramt retries

--- format_parser.py
+++ format_parser.py
@@ -11,31 +11,31 @@ class Fields:
 
     mthd = r"(GET|HEAD|PUT|POST|DELETE|PATCH|OPTIONS)"
     url = r"(\/([a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\\;\/\,\?\:\@\=\&\%]+)?|\*)"
-    rfr = r"((?:http[s]?:\/\/.)((?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)(|:\d)|((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}|" + a_v6 + r")(:(\d+\/?))?" + fr"({url})?|-)"
+    rfr = r"https?\:\/\/(([0-9a-z\-]+\.){1,8}([a-zA-Z]+|xn--p1ai)|" + a_v4 + "|" + a_v6 + r")(:\d+)?/?" + rf"{url}?" 
     http = r"HTTP\/[0123].[012689]"
     ip = rf"({a_v4}|{a_v6})"
-    x_for = rf"(-|{ip}(, {ip})?)"
+    x_for = rf"{ip}(, {ip})?"
     sts = r"[1-5][0-9][0-9]"
-    byt = r"(\d+|-)"
+    byt = r"\d+|"
     agnt = r'''(?:(?!\").)*'''
     datim = r"[0-9][0-9]\/[A-Z][a-z][a-z]\/202[0-9]:[0-9][0-9]:[0-9][0-9]:[0-9][0-9]"
-    timzn = r"(\+|\-)0[0-9]00"
-    upstrm = r"(\d+.\d+|\-)"
-    usr = r"(-|\w+)"
+    timzn = r"(\+|\-)[0-9][0-9]00"
+    upstrm = r"\d+.\d+"
+    usr = r"\w+"
 
     mthd = fr"(?P<mthd>{mthd})"
     url = fr"(?P<url>{url})"
-    rfr = fr"(?P<rfr>{rfr})"
+    rfr = fr"(?P<rfr>(\-|{rfr}))"
     http = fr"(?P<http>{http})"
     ip = fr"(?P<ip>{ip})"
-    x_for = fr"(?P<x_for>{x_for})"
+    x_for = fr"(?P<x_for>(\-|{x_for}))"
     sts = fr"(?P<sts>{sts})"
-    byt = fr"(?P<byt>{byt})"
+    byt = fr"(?P<byt>(\-|{byt}))"
     agnt = fr"(?P<agnt>{agnt})"
     datim = fr"(?P<datim>{datim})"
     timzn = fr"(?P<timzn>{timzn})"
-    upstrm = fr"(?P<upstrm>{upstrm})"
-    usr = fr"(?P<usr>{usr})"
+    upstrm = fr"(?P<upstrm>(\-|{upstrm}))"
+    usr = fr"(?P<usr>(\-|{usr}))"
 
     combined = fr'''{ip} - {usr} \[{datim} {timzn}\] \"{mthd} {url} {http}\" {sts} {byt} \"{rfr}\" \"{agnt}\"\n?'''
     bitrixvm_main = fr'''{ip} - {usr} \[{datim} {timzn} - {upstrm}\] {sts} \"{mthd} {url} {http}\" {byt} \"{rfr}\" \"{agnt}\" \"{x_for}\"\n?'''
@@ -74,20 +74,11 @@ class Format():
     class  Exception(Exception):
         pass
 
-    def __init__(self,sample_line:str=None,name:str="combined"):
-        if sample_line:
-            log.debug("Format with sample_line")
-            _, self.log_format,self.date_format,self.time_format,self.name = Format.match_line(sample_line)
-            return
-        if name:
-            log.debug("Format with name")
-            for  format_type in format_list:
-                if format_type[4] == name :
-                    _, self.log_format,self.date_format,self.time_format,self.name = format_type
-                    return
-            raise Format.Exception(f"unknown format name: {name}")
+    def __init__(self,_format:tuple=format_list[0]):
+        _, self.log_format,self.date_format,self.time_format,self.name = _format
+        
     @staticmethod
-    def match_line(sample_line:str):
+    def _match_line(sample_line:str):
 
         for format_type in format_list:
             pattern = re.compile(format_type[0])
@@ -97,32 +88,37 @@ class Format():
             if match:
                 return format_type
 
-        raise Format.Exception(f"unknown format line: {sample_line}")
+        return [None] * 5
 
     @staticmethod
-    def get_format(log_strings:list[str],args:dict ):
-        name = args['fmt']
-        res_format:Format=None
+    def get_format_by_name(log_strings:list[str],name:str):
         if name:
             log.debug(f"trying {name} log format")
-            res_format = Format(name=name)
-        else:
-            log.debug("trying to deduce log format")
-            best_sample_line: str = ""
-            best_sample_line_num: int = 0
-            count = len(log_strings)
-            best_sample_count: int  = sys.maxsize
-            for line_num in range(count):
-                line = log_strings[line_num]
-                count = line.count('"-"')
-                if best_sample_count > count:
-                    best_sample_line_num = line_num
-                    best_sample_line = line
-                    best_sample_count = count
-            log.debug(f"best sample line {best_sample_line_num+1} {best_sample_line}")
-            res_format =   Format(sample_line=best_sample_line)
+            for  format_type in format_list:
+                if format_type[4] == name :
+                    return Format(format_type)
+            raise Format.Exception(f"unknown format name: {name}")
+
+
+    @staticmethod
+    def get_format(log_strings:list[str],translate:bool ):
+        res_format:Format=None
+        log.debug("Format with sample_line")
+                      
+        split:int = 8
+        step = int(len(log_strings)/split)
         
-        if args['trnslt']:
+        log.debug(f"testing lines:")
+        for index in range(split):
+            line = log_strings[int(index*step)]
+            log.debug(line)
+            res_format = Format(Format._match_line(line))
+            if res_format.name:
+                return res_format
+            
+        raise Format.Exception(f"unknown format")
+
+        if translate:
             res_format.log_format = format_translated_real_ip.get(res_format.name,res_format.log_format)
         
         return res_format
