feb1da25280bd34a10005279e9147e095835c65e
papaya <papaya@papaya.Dlink>
2026-04-06 23:42:03 +0300

    read config from file + pre moving to swarm

--- cache.py
+++ cache.py
@@ -1,33 +1,17 @@
 import redis
 
-from settings import  Settings
-from utility import Logger as log
-
+from settings import Settings
 
 class Cache_Server:
 
-    active:bool=True
 
     redis:redis.client.Redis
+
     def __init__(self):
-
-        if not Settings.cache:
-            self.active:bool =False
-            return
-
-        try:
-            self.redis = redis.Redis(host=Settings.cache_srv, port=Settings.cache_port, db=0)
-            self.set('test','testtest')
-            _:str = self.get('test')
-        except Exception as _:
-            log.warn(f"unable to connect to ${Settings.cache_srv}:${Settings.cache_port}.\nCaching diabled")
-            self.active = False
+        addr, port = tuple(Settings.cache_server.split(":",1))
+        self.redis = redis.Redis(host=addr, port=port, db=0)
 
     def get(self,key: str):
-        if not self.active:
-            return None
         return self.redis.get(key)
     def set(self,key: str, html : str):
-        if not self.active:
-            return None
         return self.redis.set(key,html)
